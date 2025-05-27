035c6391f20e28198706ff244867e5b127ece076
Talia <nataliakaczorowska44@gmail.com>
2025-05-27 20:38:03 +0100

    the first commit

---
+++ studentTeacherOOP/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentTeacherOOP
+{
+    class Person
+    {
+        public string Age { get; set; } // property for age
+        public void Greet()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void SetAge(int age)
+        {
+            //assign age of person
+            //get set thingy somewhere here
+
+            Console.WriteLine("Input age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Age = age.ToString(); // convert int to string and assign to Age property
+            //check if it works without the property afterwards
+
+        }
+    }
+}
---
+++ studentTeacherOOP/Professor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentTeacherOOP
+{
+    class Professor : Person
+    {
+        public void Explain()
+        {
+            Console.WriteLine("I am explaining.");
+        }
+    }
+}

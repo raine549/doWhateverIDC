af9345cecefb5af4a0a2b0ec302030a4ab270f90
Talia <nataliakaczorowska44@gmail.com>
2025-06-01 18:57:00 +0100

    third commit but this time from cmd lets see whatll happen

---
+++ PhotoBookOOP/BigPhotoBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoBookOOP
+{
+    class BigPhotoBook
+    {
+        private int numPages;
+
+        public BigPhotoBook() // Constructor
+        {
+            numPages = 64; // Default number of pages for a big photo book
+        }
+
+        public int GetNumPages()
+        {
+            return numPages; // Return the number of pages
+        }
+    }
+}
---
+++ PhotoBookOOP/PhotoBook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PhotoBookOOP
+{
+    class PhotoBook
+    {
+        private int numPages;
+        public int NumPages
+        {
+            get { return numPages; }
+            set
+            {
+                if (value < 1) // Ensure that the number of pages is at least 1
+                {
+                    throw new ArgumentException("Number of pages must be at least 1.");
+                }
+                numPages = value; 
+            }
+        }
+        public int GetNumPages()
+        {
+            return NumPages;
+        }
+
+
+        public PhotoBook() // Default constructor
+        {
+            numPages = 16;
+        }
+
+        public PhotoBook(int numPages2) // second constructor
+        {
+          numPages = numPages2; // Allow user to set the number of pages
+        }
+    }
+}
